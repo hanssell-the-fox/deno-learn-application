914a2ed1ed0aa6b70b3eaedf7e6018bf5d8c6f0a
Wederson de Oliveira Calixto <wederson.programador@gmail.com>
2022-04-08 23:16:15 -0300

    Added logging middleware

--- mod.ts
+++ mod.ts
@@ -7,30 +7,29 @@ app.use(async ({ request }, next) => {
   await next();
   console.log(`${request.method} ${request.url}`);
 });
-/*
+
 app.use(async ({ response }, next) => {
   const start = Date.now();
   await next();
   const delta = Date.now() - start;
   response.headers.set("X-Response-Time", `${delta}`);
-});*/
+});
 
 app.use(async (ctx) => {
   const filePath = ctx.request.url.pathname;
-
-/*  const fileWhitelist = [
+  const fileWhitelist = [
     "/index.html",
     "/images/favicon.png",
     "/javascripts/script.js",
     "/stylesheets/style.css",
   ];
-*/
-  const folder:SendOptions = {
+
+  const publicFolder: SendOptions = {
     root: `${Deno.cwd()}/public/`,
     index: "index.html"
   }
 
-  await send(ctx, filePath, folder);
+  await send(ctx, filePath, publicFolder);
 });
 
 app.use(({ response }) => {
